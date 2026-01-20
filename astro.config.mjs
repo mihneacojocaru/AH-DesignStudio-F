b3db0fc45a32b320511f3bb13ef3e2ec22981b9d
Mihnea Cojocaru <mcojocaru@uandi.com>
2026-01-20 19:18:19 +0100

    Add minimalistic Sanity Connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 import alpinejs from '@astrojs/alpinejs';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), alpinejs()],
+    integrations: [
+        mdx(), 
+        sitemap(), 
+        alpinejs(), 
+        sanity({
+            projectId: '2cpcrc28',
+            dataset: 'production',
+            useCdn: false,
+            apiVersion: '2024-01-01',
+            token: 'skI46jNOj3JuqSCjfSOq1ImkKpRqt7nA99zaFUIrGF73CyspkQfJKKZfH9dIpZoGQwwQCwotk9jPMkkwsKBmJvBlWwUU4xIqlygcSy45iDazwSUZXLQFVQCV5ErdKGxzwrgka4UgO7JpwrPh3kauM8tuMdKAWffeX1uJ780NJ51JNTfunZXm',
+        })
+    ],
     vite: {
         plugins: [tailwindcss()],
     },
